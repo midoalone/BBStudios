09a1738bd4f7d36b8a32431b12a1f17f27462548
midoalone <mido_alone2001@yahoo.com>
2017-12-01 22:47:42 +0200

    instant json update + number spinner

--- components/number-spinner/component.js
+++ components/number-spinner/component.js
@@ -5,14 +5,10 @@ component('numberSpinner', {
     templateUrl: './components/number-spinner/template.html',
     bindings: {
         name: '@',
-        parent: '@'
+        title: '@'
     },
     transclude: true,
     controller: function ($scope, $rootScope) {
-        var self = this;
-        $scope.change = function ($id){
-            if(!$rootScope.json[self.name]) $rootScope.json[self.name] = {};
-            $rootScope.json[self.name][$id] = $scope.count;
-        };
+
     }
 });
